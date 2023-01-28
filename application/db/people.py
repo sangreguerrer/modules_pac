97d3afdb6a1bc07f3b263de52b499d46cb737da2
sangreguerrer <104795973+sangreguerrer@users.noreply.github.com>
2023-01-28 23:41:47 +0300

    Update people.py

--- application/db/people.py
+++ application/db/people.py
@@ -1,1 +1,9 @@
-
+employees=['Виталий Гармохин','Марк Безус', 'Николай Рабочий','Антон Мельник', 'Степан Горилкин']
+def get_employees():
+    while employees:
+        name=input('Введите имя сотрудника:')
+        if name in employees:
+            print('Есть такой')
+        else:
+            print('На больничном')
+        return ''
